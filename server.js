f3dbbbdc370e319af75279b77291351498ed5622
akshita <ajudiyaakshita5@gmail.com>
2024-12-17 16:50:40 +0530

    movie

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const connectDB = require("./db/dbconnect");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Database connection file
+app.use("/v1", routes);
+
+
+connectDB();
+
+http.createServer(app).listen(process.env.PORT, () => {
+  console.log("server started");
+});
